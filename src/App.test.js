2ba06812f93108d30eac9220c7474d5270458173
Glenn <a810.glenn@gmail.com>
2022-09-22 19:29:06 +0900

    TDD 개발

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('the counter starts at 0', () => {
+  render(<App />)
+
+  const counterElement = screen.getByTestId('counter')
+  expect(counterElement).toHaveTextContent(0)
+})
